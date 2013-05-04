3e5aa5cec14c3df00bd1166b3defc2c58b4de29a
Dennis Ruhe <dennis@amazingsystems.nl>
2013-05-05 01:29:59 +0200

    Fixed bugs in EAV object

--- classes/Kohana/EAV.php
+++ classes/Kohana/EAV.php
@@ -14,18 +14,26 @@ class Kohana_EAV extends ORM {
 	
 	protected $_attributes_loaded = FALSE;
 	
+	public static function factory($model, $id = NULL)
+	{
+		// Set class name
+		$model = 'Model_'.$model;
+
+		return new $model($id);
+	}
+	
 	public function __construct($id)
 	{
 		parent::__construct($id);
 		
 		if( ! $this->_attributes_table_name)
 		{
-			$this->_attributes_table_name = str_replace('Model_', '', get_class($this)) .'_attributes';
+			$this->_attributes_table_name = strtolower(str_replace('Model_', '', get_class($this)) .'_attributes');
 		}
 		
 		if( ! $this->_values_table_name)
 		{
-			$this->_values_table_name = str_replace('Model_', '', get_class($this)) .'_attribute_values';
+			$this->_values_table_name = strtolower(str_replace('Model_', '', get_class($this)) .'_attribute_values');
 		}
 		
 		if(count($this->_attributes_table_columns) == 0)
@@ -49,6 +57,8 @@ class Kohana_EAV extends ORM {
 	
 	protected function _get_attributes()
 	{
+		$this->_attributes_loaded = TRUE;
+		
 		$result = DB::select(
 				array($this->_attributes_table_name .'.'. Arr::get($this->_attributes_table_columns, 'id'), 'id'),
 				array($this->_attributes_table_name .'.'. Arr::get($this->_attributes_table_columns, 'name'), 'name'),
@@ -57,48 +67,34 @@ class Kohana_EAV extends ORM {
 		)->from($this->_values_table_name)
 		 ->join($this->_attributes_table_name)->on($this->_attributes_table_name .'.'. Arr::get($this->_attributes_table_columns, 'id'), '=', $this->_values_table_name .'.'. Arr::get($this->_values_table_columns, 'attribute_id'))
 		 ->where(Arr::get($this->_attributes_table_columns, 'item_id'), '=', $this->id)
-		 ->execute()
-		 ->as_object();
+		 ->as_object()
+		 ->execute();
 		
 		foreach($result as $property)
 		{
 			settype($property->value, $property->type);
-			$this->{$property->name} = $property->value;
+			$this->attr($property->name, $property->value);
 		}
-		
-		$this->_attributes_loaded = TRUE;
 	}
 	
-	public function __set($column, $value)
+	public function attr($column, $value = NULL)
 	{
-		if(array_key_exists($column, $this->_table_columns))
+		// Check if the attributes are loaded
+		if( ! $this->_attributes_loaded)
 		{
-			parent::__set($column, $value);
+			$this->_get_attributes();
 		}
-		else 
+		
+		if($value)
 		{
-			// Check if the attributes are loaded
-			if( ! $this->_attributes_loaded)
-			{
-				$this->_get_attributes();
-			}
-			
 			// Get the attribute ID
 			$id = Arr::get($this->_eav_object, $column);
 			
 			$this->_eav_object[$column] = array(
 					'id'    => $id,
-					'type'  => typeof($value),
+					'type'  => gettype($value),
 					'value' => $value,
 			);
-		}
-	}
-	
-	public function __get($column)
-	{
-		if(array_key_exists($column, $this->_table_columns))
-		{
-			parent::__get($column, $value);
 		}
 		else
 		{
@@ -107,7 +103,7 @@ class Kohana_EAV extends ORM {
 			{
 				$this->_get_attributes();
 			}
-				
+		
 			// Get the attribute value
 			return Arr::get(Arr::get($this->_eav_object, $column), 'value');
 		}
