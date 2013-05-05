439d04f6f866a8c6eb22365859f9cd58f19aaad8
Dennis Ruhe <dennis@amazingsystems.nl>
2013-05-05 11:57:40 +0200

    Added license information

---
+++ LICENSE.md
@@ -0,0 +1,20 @@
+Entity-attribute-value model (EAV) is a data model to describe entities where the number of attributes (properties, parameters) that can be used to describe them is potentially vast, 
+but the number that will actually apply to a given entity is relatively modest. In mathematics, this model is known as a sparse matrix. 
+EAV is also known as object-attribute-value model, vertical database model and open schema.
+
+http://en.wikipedia.org/wiki/Entity-attribute-value_model
+
+Copyright (C) 2013 Dennis Ruhe
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- classes/Kohana/EAV.php
+++ classes/Kohana/EAV.php
@@ -1,5 +1,21 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
+/**
+ * Entity–attribute–value model (EAV) is a data model to describe entities where the number of attributes (properties, parameters) that can be used to describe them is potentially vast, 
+ * but the number that will actually apply to a given entity is relatively modest. In mathematics, this model is known as a sparse matrix. 
+ * EAV is also known as object–attribute–value model, vertical database model and open schema.
+ * 
+ * There are certain cases where an EAV schematic is an optimal approach to data modelling for a problem domain. 
+ * However, in many cases where data can be modelled in statically relational terms an EAV based approach is an anti-pattern which can lead to longer development times, 
+ * poor use of database resources and more complex queries when compared to a relationally-modelled data schema.
+ * 
+ * [ref-eav] http://en.wikipedia.org/wiki/Entity-attribute-value_model
+ * 
+ * @package Kohana/eav
+ * @author Dennis Ruhe
+ * @copyright (c) 2013-2013 Dennis Ruhe
+ * @license see LICENSE.md
+ */
 class Kohana_EAV extends ORM {
 	
 	protected $_attributes_table_name = NULL;
