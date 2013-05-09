9579af894cf9813b4fbf92164f906566912bfe7b
Dennis Ruhe <dennis@amazingsystems.nl>
2013-05-09 15:17:04 +0200

    Fixed markup sample-sql

--- sample-scheme-mysql.sql
+++ sample-scheme-mysql.sql
@@ -7,24 +7,25 @@
 ## TABLE STRUCTURE FOR products
 ##
 CREATE TABLE IF NOT EXISTS `products` (
-	`id`					INT(11) UNSIGNED	NOT NULL	AUTO_INCREMENT,
-	`name`					VARCHAR(500)		NOT NULL,
-	`description`			TEXT				NOT NULL	DEFAULT '',
+	`id`           INT(11) UNSIGNED  NOT NULL   AUTO_INCREMENT,
+	`name`         VARCHAR(500)      NOT NULL,
+	`description`  TEXT              NOT NULL   DEFAULT '',
 	
-	PRIMARY KEY 	(`id`)
+	PRIMARY KEY    (`id`)
+	
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ##
 ## TABLE STRUCTURE FOR product_attributes
 ##
 CREATE TABLE IF NOT EXISTS `product_attributes` (
-	`id`			INT(11) UNSIGNED	NOT NULL	AUTO_INCREMENT,
-	`product_id`	INT(11) UNSIGNED	NOT NULL,
-	`type`			VARCHAR(500)		NOT NULL,
-	`name`			VARCHAR(500)		NOT NULL,
+	`id`          INT(11) UNSIGNED  NOT NULL    AUTO_INCREMENT,
+	`product_id`  INT(11) UNSIGNED  NOT NULL,
+	`type`        VARCHAR(500)      NOT NULL,
+	`name`        VARCHAR(500)      NOT NULL,
 	
-	PRIMARY KEY			(`id`),
-	INDEX				(`product_id`),
+	PRIMARY KEY    (`id`),
+	INDEX          (`product_id`),
 	
 	CONSTRAINT `product_attributes_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE
 	
@@ -34,10 +35,10 @@ CREATE TABLE IF NOT EXISTS `product_attributes` (
 ## TABLE STRUCTURE FOR product_attribute_values
 ##
 CREATE TABLE IF NOT EXISTS `product_attribute_values` (
-	`product_attribute_id`	INT(11) UNSIGNED	NOT NULL,
-	`value`					VARCHAR(500)		NOT NULL,
+	`product_attribute_id`  INT(11) UNSIGNED   NOT NULL,
+	`value`                 VARCHAR(500)       NOT NULL,
 	
-	INDEX				(`product_attribute_id`),
+	INDEX    (`product_attribute_id`),
 	
 	CONSTRAINT `product_attribute_values_ibfk_1` FOREIGN KEY (`product_attribute_id`) REFERENCES `product_attributes` (`id`) ON DELETE CASCADE
 	
